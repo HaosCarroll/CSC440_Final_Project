3de136596d88a4293055457b3e033af67d5842cf
KoreaHaos <koreahaos@gmail.com>
2016-11-18 21:37:39 +0000

    Fix MongoDB startup script, again...

--- z_scripts/setup_mongo_db_on_cloud_9.bash
+++ z_scripts/setup_mongo_db_on_cloud_9.bash
@@ -8,7 +8,7 @@ function create_mongo_db_startupscript(){
 mkdir -p $working_directory/data/log/
 clear
 printf "\nStarting MongoDB!!\n\n"
-mongod --bind_ip=$IP --dbpath=data --nojournal --rest "$@" --fork --logpath=$working_directory/data/log/mongodb.log
+mongod --bind_ip=$IP --dbpath=data --nojournal --fork --logpath=$working_directory/data/log/mongodb.log
 printf "\n\nUse VM kill process to stop until something better gets figured out...\n"
 
 EOT
